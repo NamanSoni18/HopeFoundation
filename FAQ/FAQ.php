6d8f8b10b12c6c85a6f24b3cf1c767f9b1537396
Naman Soni <sknaman763@gmail.com>
2023-12-27 23:04:02 +0530

    Updated FAQ Page

--- FAQ/FAQ.php
+++ FAQ/FAQ.php
@@ -7,9 +7,13 @@
     <title>Frequently Asked Quesion</title>
     <link rel="icon" href="../assests/Hope_Foundation_logo2.png" sizes="192X192" type="image/png">
     <link rel="stylesheet" href="FAQ.css">
+    <link rel="stylesheet" href="../backtotop.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
 <body>
+
+    <section id="backtotop"></section>
     <!-- Nav Bar Load -->
     <div w3-include-html="../navbar/nav.php" style="position: sticky; top: 0; background-color: #e88730; z-index: 1000">
     </div>
@@ -19,11 +23,94 @@
         includeHTML();
     </script>
 
+    <section class="back-section-div">
+        <a href="#backtotop">
+            <div id="backtotop-div">
+                <div>Back to Top</div>
+                <!-- <img src="../assests/icons/system-regular-11-arrow-up.svg" alt="" width="50px" height="50px"> -->
+                <i class="fa-solid fa-arrow-up"></i>
+            </div>
+        </a>
+    </section>
+
     <div class="faq-main">
         <div class="content">
             <h1>Frequently Asked Questions</h1>
             <p>Here are some common questions that you might have. If you don't find what you're
-                looking for, please  <a href="../connect/contact.php">write to us</a> and we'll be happy to address your query!</p>
+                looking for, please <a href="../connect/contact.php">write to us</a> and we'll be happy to address your
+                query!</p>
+        </div>
+
+        <div class="faq">
+            <div class="ques-section">
+                <h1 class="faq-ques">How will my small contribution make a difference?</h1>
+                <p class="faq-ans">Leena Sharma, our founder, started Hope Foundation with just Rs. 50. Today, we're an
+                    organisation that has impacted millions of children across India. But we could never have done this
+                    without YOU. Every drop in the ocean counts. No matter how small you think your contribution may be,
+                    we assure you that it'll make a big difference to their lives. Donate to NGO to support
+                    underprivileged children, human for provide better education and overall development.</p>
+            </div>
+
+            <div class="ques-section">
+                <h1 class="faq-ques">Does Hope Foundation accept cash donations?</h1>
+                <p class="faq-ans">No - we do not accept any cash donations in the interest of transparency
+                    and accountability. If you'd like to donate, you can do so through any of the
+                    ways listed here.</p>
+            </div>
+
+            <div class="ques-section">
+                <h1 class="faq-ques">Are donations to CRY tax exempt?</h1>
+                <p class="faq-ans">Yes - all donations to CRY are 50% exempt from tax under section 80G of
+                    the Income Tax Act. Tax exemption is valid only in India.</p>
+            </div>
+
+            <div class="ques-section">
+                <h1 class="faq-ques">Are my online donation transactions secure?</h1>
+                <p class="faq-ans">Absolutely, We have stringent security measures in place to ensure that critically
+                    sensitive information, such as your personal information and your credit/debit card details, are
+                    protected.</p>
+            </div>
+            <div class="ques-section">
+                <h1 class="faq-ques">Does Hope Foundation receive government funding?</h1>
+                <p class="faq-ans">As an NGO, we do not receive any direct government funding. However, the government
+                    has extended certain tax and duty exemptions to us to help us minimise our costs.</p>
+            </div>
+            <div class="ques-section">
+                <h1 class="faq-ques">Which areas does Hope Foundation India work in?</h1>
+                <p class="faq-ans">There are three primary areas in which we run programmes, namely education, health,
+                    livelihoods.</p>
+            </div>
+            <div class="ques-section">
+                <h1 class="faq-ques">How will my donation make a difference?</h1>
+                <p class="faq-ans">The money collected from our fundraising initiatives are used for:
+                    Empowering women and girls from poor and marginalised communities to rise above poverty and social
+                    exclusion of-school girls, and improving mental health, facilitating sustainable livelihoods for
+                    project
+                    participants, and reducing malnutrition among marginalised families Bringing about a more gender
+                    equal society, build resilience among communities to overcome exclusion, poverty and disasters, and
+                    connect women entrepreneurs to markets Strengthening healthcare and education delivery systems so
+                    that benefits reach the last mile Building capacity of teachers, anganwadi workers and other
+                    functionaries to be able to reach quality
+                    service on time to the project participants.</p>
+            </div>
+            <div class="ques-section">
+                <h1 class="faq-ques">How do you keep a donor's personal information secure?</h1>
+                <p class="faq-ans">We value the support provided by our donors. Therefore, their privacy concerns
+                    regarding
+                    the safety of their personal information is of paramount importance. The donor data is
+                    not exposed or shared with any external agencies.</p>
+            </div>
+            <div class="ques-section">
+                <h1 class="faq-ques">Can I get the details of the beneficiaries of my donation?</h1>
+                <p class="faq-ans">We regret that we do not share the personal details of the beneficiaries with our
+                    donors. The money collected from our fundraising initiatives are pooled together, and then utilised
+                    among the programmes according to the needs and urgency. Your contribution does not get directed to
+                    a single beneficiary.</p>
+            </div>
+            <div class="ques-section">
+                <h1 class="faq-ques">Does Hope Foundation accept donations in foreign currency?</h1>
+                <p class="faq-ans">No</p>
+            </div>
         </div>
     </div>
 </body>
