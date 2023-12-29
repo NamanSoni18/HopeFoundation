0051092a3b237c9b05e044f79bd4f7e579088564
Naman Soni <sknaman763@gmail.com>
2023-12-29 21:22:22 +0530

    Updated Donation page with all front-end and back-end functionalities

--- Donate/Donate.php
+++ Donate/Donate.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +11,7 @@
     <link rel="stylesheet" href="Donate.css">
     <link rel="icon" href="../assests/Hope_Foundation_logo2.png" sizes="192X192" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="../login/style.css">
     <style>
         body {
             background: url("../assests/backgrounds/background7.svg") no-repeat;
@@ -26,48 +30,6 @@
                 // return false;
             }
         }
-
-        // function resetButtons() {
-        //     var buttons = document.querySelectorAll(".amount-btn");
-        //     buttons.forEach(function (button) {
-        //         button.style.color = "#ff7801";
-        //         button.style.backgroundColor = "transparent";
-        //     });
-        // }
-
-        // function showHiddenInput(button) {
-        //     var hiddenInput = button.querySelector(".hidden-input");
-        //     if (hiddenInput) {
-        //         hiddenInput.style.display = "block";
-        //     }
-        // }
-
-        // function hideAllHiddenInputs() {
-        //     var hiddenInputs = document.querySelectorAll(".hidden-input");
-        //     hiddenInputs.forEach(function (input) {
-        //         input.style.display = "none";
-        //     });
-        // }
-
-        // function btnclick(button) {
-        //     resetButtons();
-        //     button.style.color = "whitesmoke";
-        //     button.style.backgroundColor = "#ff7801";
-
-        //     hideAllHiddenInputs();
-        //     showHiddenInput(button);
-        // }
-
-        // var buttons = document.querySelectorAll(".amount-btn");
-        // buttons.forEach(function (button) {
-        //     button.addEventListener("click", function () {
-        //         btnclick(button);
-        //     });
-        // });
-
-
-
-
     </script>
 
 </head>
@@ -87,19 +49,74 @@
     <div class="begin">
         <h1>Begin 2024 by Helping India</h1>
     </div>
-
-    <div class="form-container">
+    <div class="form-container2">
         <p class="title">Donate</p>
         <form class="form" method="post" action="" autocomplete="off">
+            <div class="name content">
+                <label for="name">
+                    <h2 class="heading">Enter Your Name: </h2>
+                </label>
+                <input type="text" name="fname" id="name" placeholder="Name" required>
+            </div>
+            <div class="email content">
+                <label for="email">
+                    <h2 class="heading">Enter Your Email ID: </h2>
+                </label>
+                <input type="email" name="email" id="email" placeholder="Email ID" required>
+            </div>
+            <div class="phone content">
+                <label for="phone_no">
+                    <h2 class="heading">Enter Your Phone Number: </h2>
+                </label>
+                <input type="number" name="phone_no" id="phone_no" placeholder="Phone Number" required>
+            </div>
+            <div class="dob content">
+                <label for="dob">
+                    <h2 class="heading">Enter Your Date of Birth: </h2>
+                </label>
+                <input type="date" name="dob" id="dob" placeholder="Date of Birth" required>
+            </div>
+            <div class="address content">
+                <label for="address">
+                    <h2 class="heading">Enter Your Address: </h2>
+                </label>
+                <textarea name="address" id="address" cols="80" rows="3" required></textarea>
+            </div>
+            <div class="zipcode content">
+                <label for="zipcode">
+                    <h2 class="heading">Enter Your Postal Code: </h2>
+                </label>
+                <input type="number" name="zipcode" id="zipcode" placeholder="Postal Code" required>
+            </div>
+            <div class="city content">
+                <label for="city">
+                    <h2 class="heading">Enter Your City: </h2>
+                </label>
+                <input type="text" name="city" id="city" placeholder="City" required>
+            </div>
+            <div class="state content">
+                <label for="state">
+                    <h2 class="heading">Enter Your State: </h2>
+                </label>
+                <input type="text" name="state" id="state" placeholder="State" required>
+            </div>
+            <div class="country content">
+                <label for="country">
+                    <h2 class="heading">Enter Your Country: </h2>
+                </label>
+                <select name="country" id="country">
+                    <option value="Indian" checked>India</option>
+                </select>
+            </div>
             <div class="nationality content">
                 <h2 class="heading">Nationality</h2>
                 <div class="choose-nation inputs">
                     <div class="indian label">
-                        <input type="radio" name="nationality" id="indian" value="Indian">
+                        <input type="radio" name="nationality" id="indian" value="Indian" required>
                         <label for="indian">Indian</label>
                     </div>
                     <div class="nri label">
-                        <input type="radio" name="nationality" id="nri" value="NRI">
+                        <input type="radio" name="nationality" id="nri" value="NRI" required>
                         <label for="nri">NRI</label>
                     </div>
                 </div>
@@ -108,15 +125,15 @@
                 <h2 class="heading">Focus Area</h2>
                 <div class="choose-focus inputs">
                     <div class="children label">
-                        <input type="radio" name="focus" id="children" value="Children">
+                        <input type="radio" name="focus" id="children" value="Children" required>
                         <label for="children">Children</label>
                     </div>
                     <div class="elders label">
-                        <input type="radio" name="focus" id="elder" value="Unprivileged Elders">
+                        <input type="radio" name="focus" id="elder" value="Unprivileged Elders" required>
                         <label for="elder">Unprivileged Elders</label>
                     </div>
                     <div class="animals label">
-                        <input type="radio" name="focus" id="animals" value="Animals">
+                        <input type="radio" name="focus" id="animals" value="Animals" required>
                         <label for="animals">Animals</label>
                     </div>
                 </div>
@@ -125,7 +142,7 @@
                 <h2 class="heading">Payment Method</h2>
                 <div class="choose-pay-meth inputs">
                     <div class="one-time label">
-                        <input type="radio" name="pay-meth" id="one-time" value="One-Time Payment">
+                        <input type="radio" name="pay-meth" id="one-time" value="One-Time Payment" required>
                         <label for="one-time">One-Time Payment</label>
                     </div>
                 </div>
@@ -135,16 +152,18 @@
                 <div class="amount">
                     <div class="Amount inputs">
                         <div class="amount label">
-                            <button onclick="btnclick(this)" type="button" class="amount-btn">
-                                <input type="radio" id="amount5k" name="price" value="5000" checked="">
+                            <button onclick="btnclick(this, '5000')" type="button" class="amount-btn"
+                                data-amount="5000">
+                                <input type="text" id="amount5k" name="price" value="5000" readonly required>
                                 <label for="amount5k"><i class="fa-solid fa-indian-rupee-sign"></i><b>5000</b></label>
                             </button>
                         </div>
                     </div>
                     <div class="Amount inputs">
                         <div class="amount label">
-                            <button onclick="btnclick(this)" type="button" class="amount-btn">
-                                <input type="radio" id="amount15k" name="price" value="15000">
+                            <button onclick="btnclick(this, '15000')" type="button" class="amount-btn"
+                                data-amount="15000">
+                                <input type="text" id="amount15k" name="price" value="15000" readonly required>
                                 <label for="amount15k"><i
                                         class="fa-solid fa-indian-rupee-sign"></i><b>15,000</b></label>
                             </button>
@@ -152,8 +171,9 @@
                     </div>
                     <div class="Amount inputs">
                         <div class="amount label">
-                            <button onclick="btnclick(this)" type="button" class="amount-btn">
-                                <input type="radio" id="amount50k" name="price" value="50000">
+                            <button onclick="btnclick(this, '50000')" type="button" class="amount-btn"
+                                data-amount="50000">
+                                <input type="text" id="amount50k" name="price" value="50000" readonly required>
                                 <label for="amount50k"><i
                                         class="fa-solid fa-indian-rupee-sign"></i><b>50,000</b></label>
                             </button>
@@ -162,7 +182,6 @@
                     <div class="Amount inputs">
                         <div class="amount label">
                             <button onclick="btnclick(this)" type="button" class="amount-btn otheramount">
-                                <input type="radio" id="Another" name="price" value="otheramount">
                                 <label for="Another"><b>Other Amount</b></label>
                             </button>
                         </div>
@@ -170,8 +189,9 @@
                 </div>
             </div>
             <div class="hidden">
-                <input type="text" class="hidden-input" placeholder="Enter amount">
+                <input type="text" name="price" id="otheramount" class="hidden-input" placeholder="Enter amount" required>
             </div>
+            <button type="submit" class="form-btn submit" name="submit">Donate ❤️</button>
         </form>
     </div>
 
@@ -192,4 +212,39 @@ function user()
 }
 
 user();
+
+$name = $email = $dob = $address = $city = $state = $country = $nationality = $focus = $payment = "";
+$amount = 100;
+if (isset($_POST['submit'])) {
+    $name = $_POST['fname'];
+    $email = $_POST['email'];
+    $phone_no = $_POST['phone_no'];
+    $dob = $_POST['dob'];
+    $address = $_POST['address'];
+    $zipcode = $_POST['zipcode'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $country = $_POST['country'];
+    $nationality = $_POST['nationality'];
+    $focus = $_POST['focus'];
+    $pay_meth = $_POST['pay-meth'];
+
+    // Check if "Other Amount" is selected
+    if (isset($_POST['price']) && $_POST['price'] == 'Other') {
+        $amount = $_POST['otheramount'];
+    } else {
+        $amount = $_POST['price'];
+    }
+
+    $query = "INSERT INTO donation(name, email, phone_no, dob, address, postalcode, city, state, country, nationality, focus, payment, amount) VALUES('$name', '$email', '$phone_no', '$dob', '$address', '$zipcode', '$city', '$state', '$country', '$nationality', '$focus', '$pay_meth', '$amount')";
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result == 1) {
+        echo '<script>alert("Donation Successfull");';
+        echo 'window.location.href="Donate.php";</script>';
+    } else {
+        echo '<script>alert("Donation Unsuccessfull");</script>';
+    }
+}
 ?>
--- Donate/script.js
+++ Donate/script.js
@@ -1,27 +1,30 @@
 function resetButtons() {
   var buttons = document.querySelectorAll(".amount-btn");
   buttons.forEach(function (button) {
-    button.style.color = "#ff7801";
-    button.style.backgroundColor = "transparent";
+      button.style.color = "#ff7801";
+      button.style.backgroundColor = "transparent";
   });
 }
 
-function btnclick(button) {
+function btnclick(button, amount) {
   resetButtons();
   button.style.color = "whitesmoke";
   button.style.backgroundColor = "#ff7801";
 
-  var hiddenInput = document.querySelector(".hidden-input");
+  var hiddenInput = document.getElementById("otheramount");
   if (button.classList.contains("otheramount")) {
-    hiddenInput.style.display = "inline-block";
+      hiddenInput.style.display = "inline-block";
+      hiddenInput.value = "";
   } else {
-    hiddenInput.style.display = "none";
+      hiddenInput.style.display = "none";
+      hiddenInput.value = amount; // Set the value of hiddenInput to the selected amount
   }
 }
 
 var buttons = document.querySelectorAll(".amount-btn");
 buttons.forEach(function (button) {
   button.addEventListener("click", function () {
-    btnclick(button);
+      var amount = button.getAttribute("data-amount");
+      btnclick(button, amount);
   });
 });
