49867da2c0f0e552bd3753b56bdc718d585fdac2
Naman Soni <sknaman763@gmail.com>
2024-01-06 11:42:00 +0530

    Updated Inbox Page

--- Inbox-admin/contact_detail.php
+++ Inbox-admin/contact_detail.php
@@ -2,16 +2,16 @@
 include "../login/connection.php";
 
 // Retrieve email from the URL parameter
-$email_cookie = $_GET['email'];
+$message_cookie = $_GET['message'];
 
 $email = $message = "";
 $time = "";
 $fname = $lname = "";
 
 // Sanitization from SQL injection
-$email = mysqli_real_escape_string($conn, $email_cookie);
+$message = mysqli_real_escape_string($conn, $message_cookie);
 
-$query = "SELECT email, message, time, fname, lname FROM contact WHERE email = '$email'";
+$query = "SELECT email, message, time, fname, lname FROM contact WHERE message = '$message'";
 
 $result = mysqli_query($conn, $query);
 
