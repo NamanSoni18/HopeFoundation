f5a33e1aa34b3d56717862375b8dc826f02c2713
Naman Soni <sknaman763@gmail.com>
2024-04-09 11:10:50 +0530

    Updated Inbox contact Page

--- Inbox-admin/contact_detail.php
+++ Inbox-admin/contact_detail.php
@@ -2,6 +2,11 @@
 include "../login/connection.php";
 session_start();
 
+if (!(isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 'admin' || $_SESSION['user_role'] == 'staff'))) {
+    echo '<script>alert("You are not an Admin or staff");';
+    echo 'window.location.href = "../../User-Panel/Main/index.php";</script>';
+}
+
 // Retrieve email from the URL parameter
 $message_cookie = $_GET['message'];
 
@@ -34,6 +39,33 @@ mysqli_close($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../navbar/nav.css">
     <title>Contact Details</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Karla:wght@489&family=Libre+Baskerville:wght@700&family=PT+Serif:wght@700&family=Protest+Riot&family=Roboto+Slab:wght@402&display=swap');
+
+        th,
+        td {
+            padding: 15px;
+            text-align: justify;
+        }
+
+        th {
+            width: 150px;
+            font-family: "Libre Baskerville", serif;
+            font-weight: 700;
+            font-style: normal;
+        }
+
+        td {
+            font-family: "Karla", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 489;
+            font-style: normal;
+        }
+
+        table {
+            margin: 30px;
+        }
+    </style>
 </head>
 
 <body>
@@ -42,18 +74,32 @@ mysqli_close($conn);
         <?php require_once("../navbar/nav.php") ?>
     </div>
 
-    <div>Name:
-        <?php echo "$name" ?>
-    </div>
-    <div>Email ID:
-        <?php echo $email ?>
-    </div>
-    <div>Time:
-        <?php echo $time ?>
-    </div>
-    <div>Message:
-        <?php echo $message ?>
-    </div>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <td>
+                <?php echo $name ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Email ID</th>
+            <td>
+                <?php echo $email ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Time</th>
+            <td>
+                <?php echo $time ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Message</th>
+            <td>
+                <?php echo $message ?>
+            </td>
+        </tr>
+    </table>
 </body>
 
 </html>
