5d5449e5c33f6cf587d19ec29564263fe1c4f40d
Naman Soni <sknaman763@gmail.com>
2024-04-09 11:16:03 +0530

    Updated

---
+++ About Us/about_us.php
@@ -0,0 +1,121 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="icon" href="../assests/Hope_Foundation_logo2.png" sizes="192X192" type="image/png">
+    <link rel="stylesheet" href="about_us.css">
+    <link rel="stylesheet" href="../Donate/Donate.css">
+    <link rel="stylesheet" href="../navbar/nav.css">
+</head>
+
+<body>
+    <!-- NavBar Load -->
+    <div style="position: sticky; top: 0; background-color: #e88730; z-index: 1000">
+        <?php require_once("../navbar/nav.php") ?>
+    </div>
+
+    <div class="color">
+        <div class="welcome">
+            <h2>Welcome to Hope Foundation - A Beacon of Compassion and Change</h2>
+            <p>At Hope Foundation, we believe in the transformative power of compassion, advocacy, and community
+                collaboration. Established with the vision of creating a world where every individual, be it a
+                child, animal, or underprivileged human, can thrive and experience a life full of hope and dignity,
+                we are committed to making a lasting impact on the lives we touch.</p>
+        </div>
+
+        <div id="line"></div>
+
+        <div class="mission about_us">
+            <div class="about-head">
+                <h1>Our Mission</h1>
+                <br>
+                <br>
+                <div class="mission-images-div">
+                    <img src="../assests/Programs/Health.jpg" alt="No Health" class="mission-img" width="100px" height="100px">
+                    <img src="../assests/Programs/Education.jpg" alt="No Health" class="mission-img" width="100px" height="100px">
+                    <img src="../assests/Programs/Livelihood.jpg" alt="No Health" class="mission-img" width="100px" height="100px">
+                </div>
+            </div>
+            <h3>Empowering Lives, Inspiring Hope:</h3>
+            <p>At the core of our mission is the commitment to empower lives and inspire hope. We strive to break the
+                cycle of despair and create sustainable positive change in communities facing adversity.</p>
+        </div>
+
+        <div id="line"></div>
+
+        <div class="about_us">
+            <div class="about-head">
+                <h1>Our Approach</h1>
+            </div>
+            <div class="content">
+                <h3>Comprehensive Solutions:</h3>
+                <p>We understand that the challenges faced by children, animals, and underprivileged humans are
+                    interconnected. Our holistic approach addresses these challenges through a range of comprehensive
+                    programs designed to meet diverse needs.
+                </p>
+            </div>
+            <div class="content">
+                <h3>Collaborative Partnerships:</h3>
+                <p>Real change happens when communities come together. We actively seek and nurture collaborations with
+                    local communities, government agencies, businesses, and like-minded NGOs to create a network of
+                    support that amplifies our impact.</p>
+            </div>
+            <div class="content">
+                <h3>Transparency and Accountability:</h3>
+                <p>Hope Foundation is dedicated to transparency and accountability in all our endeavors. We believe in
+                    keeping our supporters, donors, and the public informed about our initiatives, progress, and
+                    financial stewardship.</p>
+            </div>
+        </div>
+
+        <div id="line"></div>
+
+        <div class="about_us">
+            <div class="about-head">
+                <h1>Our Values</h1>
+            </div>
+            <div class="content">
+                <h3>Compassion:</h3>
+                <p>We approach our work with empathy, kindness, and a deep understanding of the needs of those we serve.
+                </p>
+            </div>
+            <div class="content">
+                <h3>Integrity:</h3>
+                <p>We uphold the highest standards of integrity in our actions, ensuring that our programs and
+                    initiatives are conducted ethically and responsibility.</p>
+            </div>
+            <div class="content">
+                <h3>Inclusivity:</h3>
+                <p>We believe in the inherent dignity of every individual, embracing diversity and fostering inclusivity
+                    in all our endeavors.
+                </p>
+            </div>
+            <div class="content">
+                <h3>Resilience</h3>
+                <p>In the face of challenges, we remain resilient and unwavering in our commitment to creating positive
+                    change.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="join">
+        <h2>Join us on the Journey</h2>
+        <!-- <p>Hope Foundation invites you to join us on this transformative journey. Together, we can create a world where
+            hope prevails, and every individual has the opportunity to lead a life of fulfillment and dignity.
+        </p> -->
+        <!-- <h5><b>Thank you for being a part of our mission.</b>
+        </h5> -->
+        <a href="../Donate/Donate.php">
+            <button class="Donate-btn-wrapper content-wrapper" style=" margin: 20px auto;">Contact US ❤️</button></a>
+    </div>
+
+</body>
+
+</html>
